e76b4c486281f140e761a68a4dfff216e87ff0bf
zkhiraoui <za.khiraoui@gmail.com>
2024-03-06 10:27:15 -0800

    zkhiraoui - alx  devops

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+""""Print the alphabet in reverse order alternating upper- and lower-case."""
 i = 0
 for c in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(c - i)), end="")
