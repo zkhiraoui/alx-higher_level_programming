91cc9a068b500279b02cebf154704c18edf194fb
zkhiraoui <za.khiraoui@gmail.com>
2024-03-06 10:44:06 -0800

    zkhiraoui - alx  devops

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if chr(letter) is not 'q' and chr(letter) is not 'e':
-        print("{}".format(chr(letter)), end="")
+for char in range(97, 123):
+    if chr(char) not in ['q', 'e']:
+        print("{}".format(chr(char)), end'')
