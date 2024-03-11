38d173d58147cac483d3b89132ba878a48d00549
zkhiraoui <za.khiraoui@gmail.com>
2024-03-11 10:28:19 -0700

    zkhiraoui - alx  devops

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
 
-// This line specifies the path to the interpreter for the script.
-
+/*
+ This line specifies the path to the interpreter for the script.
+*/
 function factorial (n) {
-  // This line defines a recursive function named "factorial" that calculates,
-  // the factorial of a given number "n".
+  /* This line defines a recursive function named "factorial" that calculates,
+   the factorial of a given number "n".*/
 
   if (n < 0) {
     return (-1);
-    // If "n" is negative, the function returns -1 to indicate an invalid input.
+    /*
+    If "n" is negative, the function returns -1 to indicate an invalid input.
+    */
   }
   if (n === 0 || isNaN(n)) {
     return (1);
@@ -17,12 +20,12 @@ function factorial (n) {
   }
 
   return (n * factorial(n - 1));
-  // If none of the above conditions apply, the function returns the,
-  // product of "n" and the result of recursively calling the,
-  // "factorial" function with "n - 1".
+  /* If none of the above conditions apply, the function returns the,
+     product of "n" and the result of recursively calling the,
+     "factorial" function with "n - 1".*/
 }
 
 console.log(factorial(Number(process.argv[2])));
-// This line calls the "factorial" function, passing the parsed integer value,
-// of the command - line argument as its parameter,
-// and outputs the result to the console.
+/* This line calls the "factorial" function, passing the parsed integer value,
+ of the command - line argument as its parameter,
+ and outputs the result to the console.*/
