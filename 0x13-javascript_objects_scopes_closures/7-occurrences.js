fd24caa0cb4a55453d10f17af450555bc58d1f36
zkhiraoui <za.khiraoui@gmail.com>
2024-03-12 16:07:14 -0700

    zkhiraoui - alx  devops

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,14 +1,21 @@
 #!/usr/bin/node
 
-exports.nbOccurences = function (list, searchElement) {
+// Specifies the path to the Node.js interpreter, indicating that the script should be executed with Node.js.
 
-  let nOccurrences = 0;
+exports.nbOccurences = function (list, searchElement) {
+  // Defines a function to count how many times a specific element appears in an array.
+
+  let nOccurrences = 0; 
+  // Initializes a counter to track the occurrences of the searchElement in the array.
 
   for (let i = 0; i < list.length; i++) {
+    // Loops through each element of the array to compare with searchElement.
     if (searchElement === list[i]) {
-      nOccurrences++;
+      // Checks if the current element of the array matches the searchElement.
+      nOccurrences++; // Increments the counter if a match is found.
     }
   }
 
-  return nOccurrences;
+  return nOccurrences; // Returns the total number of occurrences found.
 };
+
