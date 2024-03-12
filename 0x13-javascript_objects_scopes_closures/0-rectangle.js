72effdaaf1638fc603233acfea37fa876214fd70
zkhiraoui <za.khiraoui@gmail.com>
2024-03-12 15:51:33 -0700

    zkhiraoui - alx  devops

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 
+// The path to the interpreter for the script.
+
 class Rectangle {
+  // Class definition for the Rectangle class.
 }
 
 module.exports = Rectangle;
+// Export the Rectangle class
