2e7370bc3ce5a39d2bd93080fc9d19ca96631351
zkhiraoui <za.khiraoui@gmail.com>
2024-03-12 16:02:59 -0700

    zkhiraoui - alx  devops

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const SquareE = require('./5-square');
+
+class Square extends SquareE {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
+  }
+}
+
+module.exports = Square;
---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+let callCount = 0;
+
+exports.logMe = function (item) {
+  console.log(callCount + ': ' + item);
+
+  callCount++;
+};
