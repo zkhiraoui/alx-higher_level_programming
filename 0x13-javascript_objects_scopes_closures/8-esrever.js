ecd2f27418a524ba83138c82adba22efd5397b29
zkhiraoui <za.khiraoui@gmail.com>
2024-03-12 16:08:46 -0700

    zkhiraoui - alx  devops

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,17 +1,34 @@
 #!/usr/bin/node
 
-exports.esrever = function (list) {
+// Specifies the path to the Node.js interpreter, indicating that the script should be executed with Node.js.
 
-  let len = list.length - 1;
-  let i = 0;
+exports.esrever = function (list) {
+  // Defines a function to reverse the order of elements in an array.
+
+  let len = list.length - 1; 
+  // Initializes a variable to the last index of the array, to start swapping from the end.
+
+  let i = 0; 
+  // Initializes a variable to the first index of the array, to start swapping from the beginning.
 
   while ((len - i) > 0) {
-    const aux = list[len];
-    list[len] = list[i];
-    list[i] = aux;
-    i++;
-    len--;
+    // Continues swapping elements until it reaches the middle of the array.
+    const aux = list[len]; 
+    // Stores the current last element in a temporary variable.
+
+    list[len] = list[i]; 
+    // Swaps the last element with the first element (or the element at the current beginning index).
+
+    list[i] = aux; 
+    // Places the previously last element (stored in aux) at the current beginning index.
+
+    i++; 
+    // Moves the beginning index towards the center of the array.
+
+    len--; 
+    // Moves the end index towards the center of the array.
   }
 
-  return list;
+  return list; 
+  // Returns the array after reversing its elements.
 };
